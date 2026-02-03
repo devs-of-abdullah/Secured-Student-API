b0e932dbf6b453986224984bc00dc578506c57ad
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-02-03 23:53:18 +0300

    Implemented Cors Layer

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("APICorsPolicy", policy =>
+    {
+        policy.WithOrigins("https://localhost:7217", "http://localhost:5215")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+        
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +23,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("APICorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+ 
