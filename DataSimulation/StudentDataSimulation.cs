90cf9478c4d1920df4ef6f3b2aed305d82c59178
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-02-03 14:44:26 +0300

    Fixed to Swagger UI

--- DataSimulation/StudentDataSimulation.cs
+++ DataSimulation/StudentDataSimulation.cs
@@ -9,7 +9,7 @@ namespace StudentApi.DataSimulation
         {
             new Student { Id = 1, Name = "Albert Einstein", Age = 23,Grade=88 },
             new Student { Id = 2, Name = "Ahmad Abdullah", Age = 22,Grade=100 },
-            new Student { Id = 3, Name = "Jon Snow", Age = 21 , Grade = 66},
+            new Student { Id = 3, Name = "John Snow", Age = 21 , Grade = 66},
             new Student { Id = 4, Name = "Micheal Scoffield", Age = 19,Grade=44 }
         };
 
